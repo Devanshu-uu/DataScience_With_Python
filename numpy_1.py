12650d4f0b7117f09391026353a45d7b1ea4dc64
Devanshu <devanshumohriya@gmail.com>
2025-05-04 15:06:30 +0530

    Initial commit

---
+++ numpy_1.py
@@ -0,0 +1,241 @@
+import numpy as np 
+
+# arryy=np.array([0,1,2,3,4,5])
+# print(arryy)
+
+# zeros_array=np.zeros(6)
+# print(zeros_array)
+
+# ones_array=np.ones((2,3))
+# print(ones_array)
+
+# filled_array=np.full((2,2),7)
+# print(filled_array)
+
+# arange_array=np.arange(1,10,2)
+# print(arange_array)
+
+# identity_matrix=np.eye(3)
+# print(identity_matrix)
+
+# array_2d=np.array([[1,2,3],
+#                    [4,5,6]])
+# print(array_2d.shape)
+
+# array_size=np.array([[1,2,3],[4,5,6]])
+# print(array_size.size)
+
+# array_1d=np.array([1,2,3])
+# array_2d=np.array([[1,2],[3,4]])
+# array_3d=np.array([[[1,2],[3,4],[5,6],[7,8]]])
+
+# print(array_1d.ndim)
+# print(array_2d.ndim)
+# print(array_3d.ndim)
+
+# arry=np.array([1,23,4,3,56,43,65])
+# print(arry.dtype)
+
+# arry=np.array([1,2.3,4,3,56,43,6.5])
+# print(arry.dtype)
+
+# arry=np.array(['dev','gupil','piyush'])
+# print(arry.dtype)
+
+# arrry=np.array([1.2,3.4,5.6])
+# print(arrry.dtype)
+# aarry=arrry.astype(int)
+# print(aarry.dtype)
+
+
+# calc_array=np.array([2,3,4,5,6])
+# print(calc_array + 2)
+# print(calc_array * 10)
+# print(calc_array  - 2)
+# print(calc_array / 2)
+
+# calc_array=np.array([2,3,4,5,6])
+# print(np.sum(calc_array))
+# print(np.mean(calc_array))
+# print(np.min(calc_array))
+# print(np.max(calc_array))
+# print(np.std(calc_array))
+# print(np.var(calc_array))
+
+# indx_array=np.array([20,30,40,50,60])
+# print(indx_array[0])
+# print(indx_array[2])
+# print(indx_array[-1])
+
+
+# slic_array=np.array([10,20,30,40,50,60])
+# print(slic_array[0:5])
+# print(slic_array[:5])
+# print(slic_array[1:4])
+# print(slic_array[::2])
+# print(slic_array[::-1])
+
+
+# fancyidx_array=np.array([10,20,30,40,50,60])
+# print(fancyidx_array[[0,3,5]])
+# print(fancyidx_array[[1,2]])
+
+# boolmask_array=np.array([10,20,30,40,50,60])
+# print(boolmask_array[ boolmask_array>=20])
+
+# rray=np.array([10,20,30,40,50,60])
+# reshape_arr=rray.reshape(2,3)
+# print(reshape_arr)
+
+# arraryy=np.array([[1,2,3],[4,5,6]])
+# print(arraryy.ravel())
+# print(arraryy.flatten())
+
+
+# aray=np.array([10,20,30,40,50,60])
+# print(aray)
+# rearay=np.insert(aray,2,100)
+# print(rearay)
+
+# arrayy_2d=np.array([[1,2],[3,4]])
+# print(arrayy_2d)
+# rearrayy_2d=np.insert(arrayy_2d,2,[5,6],axis=0)
+# print(rearrayy_2d)
+# rearrayy_2d=np.insert(arrayy_2d,2,[5,6],axis=1)
+# print(rearrayy_2d)
+# rearrayy_2d=np.insert(arrayy_2d,2,[5,6],axis=None)
+# print(rearrayy_2d)
+
+# araay=np.array([10,20,30])
+# print(araay)
+# rearaay=np.append(araay,[40,50,60])
+# print(rearaay)
+
+# array1=np.array([0,1,2])
+# array2=np.array([3,4,5])
+# new_array=np.concatenate((array1,array2))
+# print(new_array)
+# new_array=np.concatenate((array1,array2),axis=0)
+# print(new_array)
+
+# arra=np.array([0,1,2,3,4,5])
+# print(arra)
+# new_arra=np.delete(arra,0)
+# print(new_arra)
+
+# arra_2d=np.array([[0,1,2],[3,4,5]])
+# print(arra_2d)
+# new_arra_2d=np.delete(arra_2d,0,axis=None)
+# print(new_arra_2d)
+# new_arra_2d=np.delete(arra_2d,0,axis=0)
+# print(new_arra_2d)
+# new_arra_2d=np.delete(arra_2d,0,axis=1)
+# print(new_arra_2d)
+
+# aarra1=np.array([0,1,2])
+# aarra2=np.array([3,4,5])
+# new_aarra1=np.vstack(((aarra1,aarra2)))
+# new_aarra2=np.hstack(((aarra1,aarra2)))
+# print(new_aarra1)
+# print(new_aarra2)
+
+# arrar=np.array([0,1,2,3,4,5])
+# print(np.split(arrar,2))
+
+
+
+
+# prices=np.array([100,200,300])
+# discount=10
+# finalprices=prices-(prices*discount/100)
+# print(finalprices)
+
+# arr=np.array([100,200,300])
+# result=arr*2
+# print(result)
+
+
+
+# arr=np.array([[1,2,3,],[4,5,6]])
+# arr2=np.array([10,20,30])
+# result=arr+arr2
+# print(result)
+
+
+
+
+# arr=np.array([[1,2,3,],[4,5,6]])
+# arr2=np.array([10,20])
+# result=arr+arr2
+# print(result)
+
+# arr=np.array([1,2,3])
+# arr2=np.array([10,20,30])
+# result=arr+arr2
+# print(result)
+
+
+
+
+# arr=np.array([4,5,6])
+# arr2=np.array([10,20,30])
+# result=arr*arr2
+# print(result)
+
+
+
+
+# arr2=np.array([10,20,np.nan,30])
+
+# print(np.isnan(arr2))
+
+
+
+
+
+# arr2=np.array([10,20,23,30])
+
+# print(np.isnan(arr2))
+
+
+# print(np.nan==np.nan)
+
+
+
+
+
+# arr2=np.array([10,20,np.nan,23,30,np.nan,np.nan])
+
+# neww=np.nan_to_num(arr2,nan=100)
+# print(neww)
+
+
+
+
+# arr2=np.array([10,20,np.nan,23,30,np.nan,np.nan])
+
+# neww=np.nan_to_num(arr2)
+# print(neww)
+
+
+
+# arr2=np.array([10,20,np.inf,23,30])
+
+# neww=np.isinf(arr2)
+# print(neww)
+
+
+
+
+
+
+# arr2=np.array([10,np.inf,20,np.nan,23,30,np.inf,np.nan,np.inf])
+
+# neww=np.nan_to_num(arr2,posinf=100,neginf=-100)
+
+# print(neww)
+
+
+
+
+
---
+++ pandas_1.py
@@ -0,0 +1,221 @@
+import pandas as pd
+
+# read = pd.read_csv("sales_data_sample.csv",encoding="latin1")
+
+# read = pd.read_excel("SampleSuperstore.xlsx")
+
+# read = pd.read_json("sample_Data.json")
+# print(read)
+
+
+# data= {
+#     "Name":['Dev','gupil','piyush'],
+#     "Age":[20,22,14],
+
+
+
+# }
+
+# dt= pd.DataFrame(data)
+# print(dt)
+
+# dt.to_csv("output.csv",index=False)
+# dt.to_excel("output.xlsx")
+# dt.to_json("output.json")
+
+# read = pd.read_json("sample_Data.json")
+
+# print("First ten rows")
+# print(read.head(10))
+
+
+# print("Second ten rows")
+# print(read.tail(10))
+
+# print("First 5 rows")
+# print(read.head())
+
+
+# print("Second 5 rows")
+# print(read.tail())
+
+
+# print("Display The Info")
+
+# print(read.info())
+
+# print("Discripton of Data")
+# print(read.describe())
+
+# data=pd.read_json("sample_Data.json")
+# # print(f'Shape: {data.shape}')
+# print(f'column: {data.columns}')
+
+# print(data['name'])
+
+# coloums= data[["name", "price"]]
+# print(coloums)
+
+
+
+# high_prices= data[data["price"]>500]
+
+# print(high_prices)
+
+# print(data['category'])
+
+# high_values= data[(data["price"]>500) & (data["price"]< 900)]
+
+# print(high_values)
+
+# high_values= data[(data["price"]<200) | (data["category"]== "Home & Kitchen")]
+
+# print(high_values)
+
+data = {
+    "name": ["Dev", "Gupil", "Piyush", "Simran", "Asha", "Manav", "Anjali"],
+    "salary": [40000, 55000, 32000,  60000, 30000, 52000, 41000],
+    "age": [22, 25, 19, 26, 21, 30, 24],
+    "performance_score": [85, 70, 92, 96, 75, 58, 89]
+}
+
+df=pd.DataFrame(data)
+# print(df)
+
+# df['bonus']= df["salary"] * 0.1
+# print(df)
+
+# df.insert(0,"Employ ID", [10,20,30,40,50,60,70,80])
+
+# print(df)
+
+# df["salary"]= df["salary"]* 1.05
+
+# df.loc[0,"salary"]=30000
+# print(df)
+
+
+
+# df.loc[0,"performance_score"]=45
+# print(df)
+
+# df.drop(columns=["performance_score"], inplace=True)
+# print(df)
+
+# print(df.isnull())
+
+
+# print(df.isnull().sum())
+
+# df.dropna(inplace=True)
+
+# df.fillna(0,inplace=True)
+
+# df["age"].fillna (df["age"].mean(),inplace=True )
+
+# df["salary"].fillna(df["salary"].mean(),inplace=True)
+
+# df["performance_score"].fillna(df["performance_score"].mean(),inplace=True)
+# print(df)
+
+
+# data1={
+
+#     "Time":[1,2,3,4,5],
+#     "Value":[10,None,30,None,50]
+
+# }
+
+
+# df1=pd.DataFrame(data1)
+
+# print(df1)
+
+# df1["Value"]=df1["Value"].interpolate(method="linear")
+
+# # print(df1)
+
+
+# df.sort_values(by="age",ascending=False,inplace=True)
+
+
+
+# df.sort_values(by="age",ascending=True,inplace=True)
+# df.sort_values(by=["age","salary"],ascending=[True,True],inplace=True)
+# print(df)
+
+# avg_salary=df["salary"].mean()
+# print(avg_salary)
+
+# min_salary=df["salary"].min()
+# print(min_salary)
+
+# sum_salary=df["salary"].sum()
+# print(sum_salary)
+
+# max_salary=df["salary"].max()
+# print(max_salary)
+
+
+# group=df.groupby("age")["salary"].sum()
+# print(group)
+
+# group=df.groupby(["age","name"])["salary"].sum()
+# print(group)
+
+
+# df_customers=pd.DataFrame({
+
+#     "id":[1,2,3],
+#     "name":["ramesh","suresh","mukesh"]
+
+# })
+
+
+# df_orders=pd.DataFrame({
+
+#     "id":[1,2,4],
+#     "orders":[220,129,344]
+    
+# })
+
+# df_merge=pd.merge(df_customers,df_orders,on="id",how="inner")
+# # print(df_merge)
+
+
+# df_merge=pd.merge(df_customers,df_orders,on="id",how="outer")
+# print(df_merge)
+
+
+# df_merge=pd.merge(df_customers,df_orders,on="id",how="left")
+# print(df_merge)
+
+
+# df_merge=pd.merge(df_customers,df_orders,on="id",how="right")
+# print(df_merge)
+
+
+
+df_region1=pd.DataFrame({
+
+    "id":[1,2,3],
+    "name":["ramesh","suresh","mukesh"]
+
+})
+
+
+df_region2=pd.DataFrame({
+
+    "id":[1,2,4],
+    "name":["manu","panu","janu"]
+    
+})
+
+# data_conta=pd.concat([df_region1,df_region2],axis=0,ignore_index=True)
+
+# print(data_conta)
+
+data_conta=pd.concat([df_region1,df_region2],axis=1,ignore_index=True)
+
+print(data_conta)
+
